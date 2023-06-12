a9d1e0dd76b27242f233ada4b2c49ca8a12ef369
EstiGluck <estiguttman@gmail.com>
2023-06-12 12:31:55 +0300

    16-12-23

--- src/Components/TableTitleRow.js
+++ src/Components/TableTitleRow.js
@@ -2,8 +2,25 @@ import './TableTitleRow.css';
 
 
 
-function TableTitleRow(){
-    return ();
+function TableTitleRow() {
+    return (
+        <tr>
+            <th>{Donations}</th>
+            <th>
+                Date
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.96004 4.47498L6.70004 7.73498C6.31504 8.11998 5.68504 8.11998 5.30004 7.73498L2.04004 4.47498" stroke="#90A3BF" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </th><th>
+                Amount
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.96004 4.47498L6.70004 7.73498C6.31504 8.11998 5.68504 8.11998 5.30004 7.73498L2.04004 4.47498" stroke="#90A3BF" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </th>
+
+            <th>{Category}</th>
+        </tr>
+    );
 
 
 }
